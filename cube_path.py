9b3dc011756936648427d5df74f84ee5e323dffe
joyce79919 <joyce79919@gmail.com>
2017-07-13 17:22:41 -0700

    Update cube_path.py

--- cube_path.py
+++ cube_path.py
@@ -1,3 +1,12 @@
 # generage a raster path to build the cubit button
 
-from math import 
+from math import *
+import numpy as np
+
+length_substrate = 2 #unit:m
+length_deposition = 0.06985 # unit: m
+
+x_0 = length_substrate/2 - length_deposition/2
+y_0 = length_substrate/2 - length_deposition/2
+
+
