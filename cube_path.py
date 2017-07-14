bb0102bc85e93b09ce4c344ca9db5ad6da0e790e
joyce79919 <joyce79919@gmail.com>
2017-07-13 23:48:54 -0700

    Update cube_path.py

--- cube_path.py
+++ cube_path.py
@@ -2,6 +2,7 @@
 
 from math import *
 import numpy as np
+import turtle
 
 length_substrate = 2 #unit:m
 length_deposition = 0.06985 # unit: m
@@ -9,4 +10,15 @@ length_deposition = 0.06985 # unit: m
 x_0 = length_substrate/2 - length_deposition/2
 y_0 = length_substrate/2 - length_deposition/2
 
-
+def go(step, heading):
+     turtle.setheading(heading)
+     turtle.forward(step)
+  
+def go_down(step):
+     go(step, 270)
+def go_up(step):
+     go(step, 90)
+def go_right(step):
+     go(step, 0) 
+def go_left(step):
+     go(step, 180)
