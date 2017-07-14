3a56b8ffe734e85da894da67cad1d063c7e73a56
joyce79919 <joyce79919@gmail.com>
2017-07-14 11:51:45 -0700

    Update cube_path.py

--- cube_path.py
+++ cube_path.py
@@ -28,24 +28,52 @@ y = substrate_length/2 - cube_length/2
 z = 0
 t = 0
 
+def get_increment(cube_length, laser_diameter):
+	increment = cube_length / (laser_diameter *1.2)
+	return increment
+
+def x_path(cube_length, laser_speed, x, y, z, t):
+	x_inc = get_increment()
+	x_path = []
+	while x < = cube_length:
+		x = x + x_inc
+		y = y
+		z = z
+		t = t + x_inc / laser_speed
+		x_path.append([t,x,y,z])
+	return x_path
+
+def y_path(laser_speed, x, y, z, t):
+	y_inc = get_increment()
+	y_path = []
+	x = x
+	y = y + y_inc
+	t = t + y_inc / laser_speed
+	z = z
+	y_inc = (-1) * y_inc
+	y_path.append([t,x,y,z])
+	return y_path
+
+def z_path(laser_speed, x, y, z, t):
+	z_inc = get_increment()
+	z_path = []
+	x = x 
+	y = y	
+	z = z + z_inc
+	return z_path
 
 def get_path(laser_diameter, cube_length, laser_speed,t,x,y,z):
 	path = []
-	x_increment = cube_length/(laser_diameter*1.2)
-	y_increment = cube_length/(laser_diameter*1.2)
-	layer = cube_length/x_increment # number of times laser needs to go upwards
+	a = get_increment()
+	layer = cube_length/a # number of times laser needs to go upwards
 
-	for i in range(total_number_steps):
-		t = t + increment/laser_speed # unit: second
-		while x < = cube_length:
-			x = x + x_increment
-			y = y
-			z = z
-			t = t + increment/laser_speed # unit: second	
-		y = y + y_increment
-		t = t + increment/laser_speed # unit: second		
-		y_increment = (-1) * y_increment
-		path.append([t,x,y.z])
+	for i in range(layer):
+		inc = get_increment()
+		while (y + inc) < = cube_length:
+			path = np.concatenate((path,x_path()))
+			path = np.concatenate((path,y_path()))
+		z_path()
+		#path.append([t,x,y.z])
 
 #print(np.shape(path))
 np.savetxt('path.txt',path)
